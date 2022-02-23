1e46718df489d0af8ebcb21ee365bfda23e1a684
Carlos Eduardo <carlos.eduardo@estudante.ifgoiano.edu.br>
2022-02-23 18:03:22 -0300

    arquivos excluídos

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
@@ -9,14 +8,13 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
-
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <ToastContainer autoClose={3000} className="toast-container" />
-    <App />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 reportWebVitals();
