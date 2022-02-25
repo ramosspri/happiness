967174f9c16e1a2be6b9de987faeeb88d9bc01b6
Priscila Ramos <oliveirapriscila90@gmail.com>
2022-02-25 00:03:35 -0300

    consegui fazer o menu ficar com hover, que orgulho

--- front-end/src/componentes/SideMenu/index.tsx
+++ front-end/src/componentes/SideMenu/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {LinkDecoration} from './styles'
+import {LinkDecoration, LinkSpaces} from './styles'
 
 export const SideMenu = () => {
   return (
     <>
+    <LinkSpaces>
       <LinkDecoration to="/">Home</LinkDecoration>
+    </LinkSpaces>
+    <LinkSpaces>
       <LinkDecoration to="/tabela">Tabela</LinkDecoration>
+    </LinkSpaces>
     </>
   );
 };
--- front-end/src/componentes/SideMenu/styles.ts
+++ front-end/src/componentes/SideMenu/styles.ts
@@ -4,5 +4,15 @@ import styled from 'styled-components';
 export const LinkDecoration = styled(Link)`
   text-decoration: none;
   color: ${({ theme }) => theme.colors.secondary};
-  font-size: ${({ theme }) => theme.font.sizes.small};
+  font-size: ${({ theme }) => theme.font.sizes.large};
+
+  &&:hover {
+    color: ${({ theme }) => theme.colors.letter_color}
+  }
 `;
+
+export const LinkSpaces = styled.div`
+  justify-content: space-between;
+  padding: 30px;
+  margin: 30px;
+`
