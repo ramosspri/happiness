bae83dbc0210fc799472c4e0625cede7bb384d41
Priscila Ramos <oliveirapriscila90@gmail.com>
2022-03-02 11:42:57 -0300

    criando o menu lateral

--- front-end/src/componentes/Header/Logo/styles.ts
+++ front-end/src/componentes/Header/Logo/styles.ts
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 
 export const ImgLogo = styled.img`
   grid-area: logo;
-  display: block;
-  justify-content: flex-end;
+  display: flex;
+  justify-content: start;
   width: 100px;
   height: 90px;
+  border: solid 1px black;
 `;
