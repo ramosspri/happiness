a36d94c80834db3b69bef11e2e5b6150684acee8
Priscila Ramos <oliveirapriscila90@gmail.com>
2022-03-03 23:55:20 -0300

    fui vencida pelo footer

--- front-end/src/componentes/Footer/styles.ts
+++ front-end/src/componentes/Footer/styles.ts
@@ -4,8 +4,8 @@ export const ContainerFooter = styled.footer`
   grid-area: footer;
   display: flex;
   justify-content: center;
-  box-shadow: 0rem -0.1875rem 0.25rem 0rem rgb(0 0 0 / 25%);
   align-items: center;
+  box-shadow: 0rem -0.1875rem 0.25rem 0rem rgb(0 0 0 / 25%);
   background-color: ${({ theme }) => theme.colors.secondary};
   color: ${({ theme }) => theme.colors.letter_color};
   height: 3.6rem;
