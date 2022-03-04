64161bf7fdfc179ade6034483c31fd22b354a733
Priscila Ramos <oliveirapriscila90@gmail.com>
2022-03-04 13:18:47 -0300

    colocando nossos gits no footer

--- front-end/src/componentes/Footer/index.tsx
+++ front-end/src/componentes/Footer/index.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
-import { ContainerFooter, DivIcon } from './styles';
-import { FaRegCopyright } from 'react-icons/fa';
+import { ContainerFooter, DivIcon, DivTextIcon, SeparateContainers } from './styles';
+import { FaRegCopyright, FaGithub } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
   return (
     <ContainerFooter>
-      <DivIcon>
-        <FaRegCopyright />
-      </DivIcon>
-      Todos os direitos reservados
+      <SeparateContainers>
+      <DivTextIcon>
+        <DivIcon>
+          <FaRegCopyright />
+        </DivIcon>
+        <p>Alguns direitos reservados</p>
+      </DivTextIcon>
+      </SeparateContainers>
+
+      <SeparateContainers>
+        <DivTextIcon>
+        <p style={{fontWeight:'bold'}}>Desenvolvido por: </p>
+        </DivTextIcon>
+        <DivTextIcon>
+          <DivIcon>
+            <a href="https://github.com/CarlosERM" style={{color:'#6B62CE'}}><FaGithub/></a>
+          </DivIcon>
+          <p>Carlos Eduardo</p>
+        </DivTextIcon>
+
+        <DivTextIcon>
+          <DivIcon>
+          <a href="https://github.com/GustavoSpm" style={{color:'#6B62CE'}}><FaGithub/></a>
+          </DivIcon>
+          <p>Gustavo Spindola</p>
+        </DivTextIcon>
+
+        <DivTextIcon>
+          <DivIcon>
+          <a href="https://github.com/ramosspri" style={{color:'#6B62CE'}}><FaGithub/></a>
+          </DivIcon>
+          <p>Priscila Ramos</p>
+        </DivTextIcon>
+      </SeparateContainers>
     </ContainerFooter>
   );
 };
--- front-end/src/componentes/Footer/styles.ts
+++ front-end/src/componentes/Footer/styles.ts
@@ -1,16 +1,18 @@
+import { NONAME } from 'dns';
 import styled from 'styled-components';
 
 export const ContainerFooter = styled.footer`
   grid-area: footer;
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: space-evenly;
+  font-family: 'Montserrat', sans-serif ;
   box-shadow: 0rem -0.1875rem 0.25rem 0rem rgb(0 0 0 / 25%);
   background-color: ${({ theme }) => theme.colors.secondary};
   color: ${({ theme }) => theme.colors.letter_color};
-  height: 3.6rem;
+  height: 6.25rem;
   z-index: 100;
-  margin-top: 50px;
+  margin-top: 3.125rem;
   background-color: ${({ theme }) => theme.colors.secondary};
   position: fixed;
   bottom: 0vh;
@@ -24,6 +26,17 @@ export const ContainerFooter = styled.footer`
   }
 `;
 
-export const DivIcon = styled.div`
-  margin: 0.625rem;
+export const DivTextIcon = styled.div`
+  display: flex;
+  margin: 2px;
 `;
+
+export const DivIcon = styled.div`
+  margin-right: 5px;
+`;
+
+export const SeparateContainers = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
